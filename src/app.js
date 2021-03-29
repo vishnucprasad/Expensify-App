d9976bfa5769189396b12133d1db276cc1067cfc
Vishnu-C-Prasad <mr.vishnucp001@outlook.com>
2021-03-29 09:57:44 +0530

    Setting Up Budget App

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(
+    <p>Expensify App</p>,
+    document.getElementById("app")
+);
