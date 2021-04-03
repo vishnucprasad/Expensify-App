1e640537a7256ff5e3bbd881ba3eba706c01a023
Vishnu-C-Prasad <mr.vishnucp001@outlook.com>
2021-04-03 09:50:34 +0530

    Wiring up Add Expense

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addExpense } from '../actions/expenses';
 import ExpenseForm from './ExpenseForm';
 
-const AddExpense = () => (
+const AddExpense = (props) => (
     <div>
         <h1>Add Expense</h1>
-        <ExpenseForm />
+        <ExpenseForm onSubmit={(expense) => {
+            props.dispatch(addExpense(expense));
+            props.history.push('/');
+        }} />
     </div>
 );
 
-export default AddExpense;
+export default connect()(AddExpense);
