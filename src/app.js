8627b1584a1d565af7a8b115365ea9f150855b70
Vishnu C Prasad <mr.vishnucp001@outlook.com>
2021-04-08 09:39:43 +0530

    Mocking Libraries with Jest

--- src/app.js
+++ src/app.js
@@ -4,15 +4,15 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water Bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas Bill' , createdAt: 1000}));
+store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000 }));
 store.dispatch(addExpense({ description: 'Rent', amount: 1095 }));
 
 const state = store.getState();
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import moment from 'moment';
 import { SingleDatePicker } from 'react-dates';
-import 'react-dates/lib/css/_datepicker.css';
 
 export default class ExpenseForm extends React.Component {
     constructor(props) {
@@ -11,7 +10,7 @@ export default class ExpenseForm extends React.Component {
             description: props.expense ? props.expense.description : '',
             note: props.expense ? props.expense.note : '',
             amount: props.expense ? (props.expense.amount / 100).toString() : '',
-            createdAt: props.expense ? moment(props.expense.createdAt) : '',
+            createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
             calenderFocused: false,
             error: ''
         };
