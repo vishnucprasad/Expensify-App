ded34e31b64d5073040aa3a2340ddc58ab9b4ee3
Vishnu C Prasad <mr.vishnucp001@outlook.com>
2021-05-04 10:47:18 +0530

    Setup test database env

--- src/tests/components/AddExpense.test.js
+++ src/tests/components/AddExpense.test.js
@@ -3,12 +3,12 @@ import { shallow } from 'enzyme';
 import { AddExpense } from '../../components/AddExpense';
 import expenses from '../fixtures/expenses';
 
-let addExpense, history, wrapper;
+let startAddExpense, history, wrapper;
 
 beforeEach(() => {
-    addExpense = jest.fn();
+    startAddExpense = jest.fn();
     history = { push: jest.fn() };
-    wrapper = shallow(<AddExpense addExpense={addExpense} history={history} />);
+    wrapper = shallow(<AddExpense startAddExpense={startAddExpense} history={history} />);
 });
 
 test('should render AddExpense correctly', () => {
@@ -18,6 +18,6 @@ test('should render AddExpense correctly', () => {
 test('should handle addExpense', () => {
     wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
     expect(history.push).toHaveBeenLastCalledWith('/');
-    expect(addExpense).toHaveBeenLastCalledWith(expenses[1]);
+    expect(startAddExpense).toHaveBeenLastCalledWith(expenses[1]);
     expect(wrapper).toMatchSnapshot();
 });
