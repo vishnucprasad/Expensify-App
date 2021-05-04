bc76b4c7e812821645827eccc5eb4ab12b99d862
Vishnu C Prasad <mr.vishnucp001@outlook.com>
2021-05-04 10:47:18 +0530

    Setup test database env

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (process.env.NODE_ENV === 'test') {
+    require('dotenv').config({ path: '.env.test' });
+} else if (process.env.NODE_ENV === 'development') {
+    require('dotenv').config({ path: '.env.development' });
+}
 
 module.exports = (env) => {
     const isProduction = !!env.production;
@@ -38,6 +47,16 @@ module.exports = (env) => {
         plugins: [
             new MiniCssExtractPlugin({
                 filename: "styles.css"
+            }),
+            new webpack.DefinePlugin({
+                'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+                'process.envFIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+                'process.envFIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+                'process.envFIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+                'process.envFIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+                'process.envFIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+                'process.envFIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
+                'process.envFIREBASE_MEASURMENT_ID': JSON.stringify(process.env.FIREBASE_MEASURMENT_ID)
             })
         ],
         devtool: isProduction ? 'source-map' : 'inline-source-map',
