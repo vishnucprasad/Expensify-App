f8f06ce9c0a72c88cca52be66ce883d37a8fbad2
Vishnu C Prasad <mr.vishnucp001@outlook.com>
2021-05-05 10:17:43 +0530

    Fetching expenses from firebase

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
+import { startSetExpenses } from './actions/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
@@ -11,8 +12,17 @@ import './firebase/firebase';
 const store = configureStore();
 
 ReactDOM.render(
-    <Provider store={store}>
-        <AppRouter />
-    </Provider>,
+    <p>
+        Loading...
+    </p>,
     document.getElementById("app")
 );
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(
+        <Provider store={store}>
+            <AppRouter />
+        </Provider>,
+        document.getElementById("app")
+    );
+});
