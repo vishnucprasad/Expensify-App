4ebe48a0c1ece00224e1f811a130a4556d2ccbd2
Vishnu C Prasad <mr.vishnucp001@outlook.com>
2021-05-11 11:04:29 +0530

    Add public routes

--- src/components/Help.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Help = () => (
-    <div>
-        This is from my help component
-    </div>
-);
-
-export default Help;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,10 +4,10 @@ import { createBrowserHistory } from 'history';
 import Dashboard from '../components/Dashboard';
 import AddExpense from '../components/AddExpense';
 import EditExpense from '../components/EditExpense';
-import Help from '../components/Help';
 import NotFonud from '../components/NotFonud';
 import Login from '../components/Login';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createBrowserHistory();
 
@@ -15,11 +15,10 @@ const AppRouter = () => (
     <Router history={history}>
         <div>
             <Switch>
-                <Route path="/" component={Login} exact={true} />
+                <PublicRoute path="/" component={Login} exact={true} />
                 <PrivateRoute path="/dashboard" component={Dashboard} />
                 <PrivateRoute path="/create" component={AddExpense} />
                 <PrivateRoute path="/edit/:id" component={EditExpense} />
-                <Route path="/help" component={Help} />
                 <Route component={NotFonud} />
             </Switch>
         </div>
