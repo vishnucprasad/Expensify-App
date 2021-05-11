f119464a74bd5362d15696d264b4f6cf4a457cad
Vishnu C Prasad <mr.vishnucp001@outlook.com>
2021-05-11 10:51:58 +0530

    Add private routes

--- src/app.js
+++ src/app.js
@@ -4,12 +4,25 @@ import { Provider } from 'react-redux';
 import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
+import { login, logout } from './actions/auth';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
 
 const store = configureStore();
+let hasRenderd = false;
+const renderApp = () => {
+    if (!hasRenderd) {
+        ReactDOM.render(
+            <Provider store={store}>
+                <AppRouter />
+            </Provider>,
+            document.getElementById("app")
+        );
+        hasRenderd = true;
+    }
+};
 
 ReactDOM.render(
     <p>
@@ -18,19 +31,19 @@ ReactDOM.render(
     document.getElementById("app")
 );
 
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(
-        <Provider store={store}>
-            <AppRouter />
-        </Provider>,
-        document.getElementById("app")
-    );
-});
-
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-        console.log('Log in');
+        store.dispatch(login(user.uid));
+        store.dispatch(startSetExpenses()).then(() => {
+            console.log(user.uid);
+            renderApp();
+            if (history.location.pathname === '/') {
+                history.push('/dashboard');
+            }
+        });
     } else {
+        store.dispatch(logout());
+        renderApp();
         history.push('/');
     }
 });
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,25 +1,24 @@
 import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import { createBrowserHistory } from 'history';
-import Header from '../components/Header';
 import Dashboard from '../components/Dashboard';
 import AddExpense from '../components/AddExpense';
 import EditExpense from '../components/EditExpense';
 import Help from '../components/Help';
 import NotFonud from '../components/NotFonud';
 import Login from '../components/Login';
+import PrivateRoute from './PrivateRoute';
 
 export const history = createBrowserHistory();
 
 const AppRouter = () => (
     <Router history={history}>
         <div>
-            <Header />
             <Switch>
                 <Route path="/" component={Login} exact={true} />
-                <Route path="/dashboard" component={Dashboard} />
-                <Route path="/create" component={AddExpense} />
-                <Route path="/edit/:id" component={EditExpense} />
+                <PrivateRoute path="/dashboard" component={Dashboard} />
+                <PrivateRoute path="/create" component={AddExpense} />
+                <PrivateRoute path="/edit/:id" component={EditExpense} />
                 <Route path="/help" component={Help} />
                 <Route component={NotFonud} />
             </Switch>
